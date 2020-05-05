996e830906394ed3da04a5e5ea14689a40d06e44
Janos Molnar <janosmolnar1001@gmail.com>
2020-05-05 20:53:33 +0200

    feat: make ball move

--- code.js
+++ code.js
@@ -4,13 +4,23 @@ function setup() {
     PADDING_BOTTOM = 20
     BALL_RADIUS = 10
     createCanvas(600, 400)
+    game_started = false
+    ball_x = width / 2
+    ball_y = height / 2
+    velocity_x = random(5)
+    velocity_y = random(5)
 }
 
 function draw() {
     background(51)
     noStroke()
 
-    ellipse(width/2, height/2, BALL_RADIUS)
+    if(game_started) {
+        ball_x = ball_x + velocity_x
+        ball_y = ball_y + velocity_y
+    }
+
+    ellipse(ball_x, ball_y, BALL_RADIUS)
     
     rect(
         constrain(mouseX, 0, width - PADDLE_WIDTH),
@@ -19,3 +29,9 @@ function draw() {
         PADDLE_HEIGHT
     )
 }
+
+function mouseClicked() {
+    if(!game_started) {
+        game_started = true
+    }
+  }
