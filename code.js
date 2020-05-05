b7a167f876fa4d40e25ff829a2c2b3059305c9bf
Janos Molnar <janosmolnar1001@gmail.com>
2020-05-05 20:41:02 +0200

    feat: add ball

--- code.js
+++ code.js
@@ -2,12 +2,15 @@ function setup() {
     PADDLE_WIDTH = 75
     PADDLE_HEIGHT = 20
     PADDING_BOTTOM = 20
-    createCanvas(600, 400);
+    BALL_RADIUS = 10
+    createCanvas(600, 400)
 }
 
 function draw() {
-    background(51);
+    background(51)
+    noStroke()
 
+    ellipse(width/2, height/2, BALL_RADIUS)
     rect(
         (width / 2) - (PADDLE_WIDTH / 2),
         400 - PADDLE_HEIGHT - PADDING_BOTTOM,
